1a38671c21d869b97449e54def0b7512952e31b7
LyceeSerie <zerryfurai@hotmail.com>
2025-12-11 03:12:59 +0900

    feature FormとWorks追加

--- SI-HP/src/views/FormPage.vue
+++ SI-HP/src/views/FormPage.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <h1>Form</h1>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfCkqBf6QmNgZIds2IXQvbmaE-6BFJxWn0vaJ6XQUGxYxyj4A/viewform">依頼受付フォーム</a>
+  <div class="form-container">
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfCkqBf6QmNgZIds2IXQvbmaE-6BFJxWn0vaJ6XQUGxYxyj4A/viewform"
+      width="640"
+      height=100%
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    >
+      読み込んでいます…
+    </iframe>
   </div>
 </template>
+
+<style scoped>
+.form-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: left;
+  padding: 30px 0 0 0;
+}
+iframe {
+  max-width: 100%;
+}
+/* Chrome, Edge, Safari */
+.form-container::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- SI-HP/src/views/WorksPage.vue
+++ SI-HP/src/views/WorksPage.vue
@@ -1,8 +1,58 @@
 <template>
-  <div>
-    <h1>WORKS</h1>
-    <a href="https://x.com/zerry_furai/status/1599378980364509185?s=20">Vket2022Winter YAMAHAブース プロップ制作</a>
-    <a href="https://x.com/zerry_furai/status/1680427035720577026?s=20">Vket2023Summer Canon ブース プロップ制作</a>
-    <a href="https://x.com/zerry_furai/status/1997215920125890946?s=20">Vket2025Winter SUZUKIブース プロップ制作</a>
+  <div class="tweet-container">
+    <div v-for="(tweet, i) in tweets" :key="i">
+      <h2>{{ tweet.title }}</h2>
+      <div v-html="tweet.html"></div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const tweets = [
+  {
+    title: "Vket2025Winter SUZUKIブース　プロップ制作",
+    html: `<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">【お仕事告知】<br>現在開催中の <a href="https://twitter.com/hashtag/Vket2025Winter?src=hash&amp;ref_src=twsrc%5Etfw">#Vket2025Winter</a> にてSUZUKI企業ブースで展示されている以下３点を製作させていただきました！<br>・SUZU-RIDE<br>・SUZU-RIDE2<br>・SUZU-CARGO<br>パラリアルシンガポールに展示されてます！ <a href="https://t.co/CX5IPup3LD">pic.twitter.com/CX5IPup3LD</a></p>&mdash; 芹井りせ/芹井産業 (@zerry_furai) <a href="https://twitter.com/zerry_furai/status/1997215920125890946?ref_src=twsrc%5Etfw">December 6, 2025</a></blockquote>`
+  },
+  {
+    title: "Vket2023Summer Canonブース　プロップ制作",
+    html: `<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">【お仕事報告】<a href="https://twitter.com/hashtag/Vket2023Summer?src=hash&amp;ref_src=twsrc%5Etfw">#Vket2023Summer</a> パラリアルラスベガス Canonブースで展示されている、カメラとレンズ５種のモデルを制作しました！<br>Canonブースは猫カフェのVR動画が最高なので見に行くべきです。 <a href="https://t.co/rWvrLxgNjJ">pic.twitter.com/rWvrLxgNjJ</a></p>&mdash; 芹井りせ/芹井産業 (@zerry_furai) <a href="https://twitter.com/zerry_furai/status/1680427035720577026?ref_src=twsrc%5Etfw">July 16, 2023</a></blockquote>`
+  },
+  {
+    title: "Vket2022Winter YAMAHAブース　プロップ制作",
+    html: `<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">【お仕事告知】<br>現在開催中の <a href="https://twitter.com/hashtag/Vket2022Winter?src=hash&amp;ref_src=twsrc%5Etfw">#Vket2022Winter</a> にてYAMAHA企業ブースで展示されている、以下４点の楽器を製作させていただきました！<br>・RevStar[YAMAHA]<br>・BBP34[YAMAHA]<br>・CATALYST200[LINE6]<br>・SVT-CL,SVT-410HLF[Ampeg]<br><br>ぜひ細部まで見てみてください！ <a href="https://t.co/9wtHgovoPp">pic.twitter.com/9wtHgovoPp</a></p>&mdash; 芹井りせ/芹井産業 (@zerry_furai) <a href="https://twitter.com/zerry_furai/status/1599378980364509185?ref_src=twsrc%5Etfw">December 4, 2022</a></blockquote>`
+  },
+]
+
+onMounted(() => {
+  if (!(window.twttr && window.twttr.widgets)) {
+    const script = document.createElement('script')
+    script.src = "https://platform.twitter.com/widgets.js"
+    script.async = true
+    script.charset = "utf-8"
+    document.body.appendChild(script)
+  } else {
+    window.twttr.widgets.load()
+  }
+})
+</script>
+
+<style lang="css" scoped>
+h2{
+  color: #000;
+  padding: 30px 0 0 0;
+}
+
+.tweet-container {
+  overflow: auto;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  width: 100%;
+}
+
+/* Chrome, Edge, Safari */
+.tweet-container::-webkit-scrollbar {
+  display: none;
+}
+</style>
