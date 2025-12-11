19aada80ce32a18a8cfa4ed81df0f39f94b4df5b
LyceeSerie <zerryfurai@hotmail.com>
2025-12-11 12:56:44 +0900

    feature 動画画面を作成

--- SI-HP/src/views/YoutubePage.vue
+++ SI-HP/src/views/YoutubePage.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <h1>Mov</h1>
-    <a href="https://www.youtube.com/user/Zerryfurai">Youtube</a>
+  <div class="youtube-page">
+    <h1></h1>
+
+    <!-- プレイリスト埋め込み -->
+    <iframe
+      width="800"
+      height="450"
+      :src="playlistUrl"
+      title="LyceeSerie Playlist"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
+
+<script setup>
+const playlistId = "PLxhxFIU5cMGKk-BRM5-OoBOmVVaAecoc0"
+const playlistUrl = `https://www.youtube.com/embed/videoseries?list=${playlistId}`
+</script>
+
+<style scoped>
+.youtube-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20% 0px;
+}
+
+iframe {
+  max-width: 100%;
+}
+</style>
